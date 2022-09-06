e3cfe05e2d4978f9f159f346b951ef54d956c819
HasithaK <hasitha.madusanka@cammsgroup.com>
2022-09-06 14:54:24 +0530

    Adding MediatR

--- DemoLibrary/DataAccess/DemoDataAccess.cs
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary.DataAccess
 {
-    public class DemoDataAccess
+    public class DemoDataAccess : IDataAccess
     {
+        private List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel { Id = 1, FirstName = "Hasitha", LastName = "Kodi" });
+            people.Add(new PersonModel { Id = 1, FirstName = "Kalpani", LastName = "Sashi" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+            p.Id = people.Max(x => x.Id) + 1;
+            people.Add(p);
+            return p;
+        }
     }
 }
